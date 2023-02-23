878a8194ec79771527346a825a0dc6a6d5fc4cb6
nicolaao <nicolaao@stud.ntnu.no>
2023-02-23 13:11:32 +0100

    added base player class

--- src/main/java/no/ntnu/idatg2001/paths/Player.java
+++ src/main/java/no/ntnu/idatg2001/paths/Player.java
@@ -1,4 +1,101 @@
 package no.ntnu.idatg2001.paths;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class for representing the player
+ * and attributes that will affect the game for the player.
+ * @author Nicolai Olsen
+ * @version 1.0-SNAPSHOT
+ */
 public class Player {
+    private final String name;
+    private int health;
+    private int score;
+    private int gold;
+    private ArrayList<String> inventory;
+
+    /**
+     * Constructor for the player class.
+     * @param name the name
+     */
+    public Player(String name, int health, int score, int gold) {
+        this.name = name;
+        this.health = health;
+        this.score = score;
+        this.gold = gold;
+    }
+
+    /**
+     * Sets the health of the player.
+     * @param health the health
+     */
+    public void addHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * Sets the score for the player.
+     * @param score the score
+     */
+    public void addScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Sets the amount of gold that the player carries.
+     * @param gold the gold
+     */
+    public void addGold(int gold) {
+        this.gold = gold;
+    }
+
+    /**
+     * method for adding items to the players inventory.
+     * @param inventory the inventory
+     */
+    public void addToInventory(ArrayList<String> inventory) {
+        this.inventory = inventory;
+    }
+
+    /**
+     * method for getting the name of the player.
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * method for getting the health of the player.
+     * @return the health
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * method for getting the players score.
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * method for getting the players gold.
+     * @return the gold
+     */
+    public int getGold() {
+        return gold;
+    }
+
+    /**
+     * method for getting the players inventory.
+     * @return the inventory
+     */
+    public List<String> getInventory() {
+        return inventory;
+    }
 }
