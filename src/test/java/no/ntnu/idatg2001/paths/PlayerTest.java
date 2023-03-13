954de4d29ee8a4fa576eacd32e50fb5f45f19dc4
nicolaao <nicolaao@stud.ntnu.no>
2023-03-13 11:42:06 +0100

    Added test to Player class

--- src/test/java/no/ntnu/idatg2001/paths/PlayerTest.java
+++ src/test/java/no/ntnu/idatg2001/paths/PlayerTest.java
@@ -1,4 +1,59 @@
 package no.ntnu.idatg2001.paths;
 
-public class PlayerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public  class PlayerTest {
+  Player player;
+
+  @BeforeEach
+  public void  createPlayet(){
+    player = new Player("Markus", 100, 20, 50);
+  }
+
+  @Test
+  void testGetters(){
+    //test getName
+    assertEquals("Markus", player.getName());
+    //Test GetHealth
+    assertEquals(100, player.getHealth());
+    //test getScore
+    assertEquals(20, player.getScore());
+    //test get gold
+    assertEquals(50, player.getGold());
+
+  }
+
+  @Test
+  void testAddHealth(){
+    player.addHealth(20);
+    assertEquals(120, player.getHealth());
+    player.addHealth(-40);
+    assertEquals(80, player.getHealth());
+  }
+
+  @Test
+  void testAddScore(){
+    player.addScore(30);
+    assertEquals(50, player.getScore());
+    player.addScore(-40);
+    assertEquals(10, player.getScore());
+  }
+
+  @Test
+  void testAddGold(){
+    player.addGold(40);
+    assertEquals(90, player.getGold());
+    player.addGold(-80);
+    assertEquals(10, player.getGold());
+  }
+
+  @Test
+  void testAddToInventory(){
+    player.addToInventory("sword");
+    assertTrue(player.getInventory().contains("sword"));
+  }
 }
