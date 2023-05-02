8a6123fb18b894e249b45c594e51fcb975c9c7db
Nicolaao <nicolaao@stud.ntnu.no>
2023-05-02 13:37:57 +0200

    Added simple testers for GoldAction.java

--- src/test/java/no/ntnu/idatg2001/paths/Actions/GoldActionTest.java
+++ src/test/java/no/ntnu/idatg2001/paths/Actions/GoldActionTest.java
@@ -1,4 +1,34 @@
 package no.ntnu.idatg2001.paths.Actions;
 
+import no.ntnu.idatg2001.paths.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GoldActionTest {
+    GoldAction goldAction;
+    Player player;
+
+    @BeforeEach
+    public void createGoldAction(){
+
+        player = new Player("Markus", 100, 20, 50);
+    }
+
+    @Test
+    void testExecute(){
+        goldAction = new GoldAction(100);
+        goldAction.execute(player);
+        assertEquals(150, player.getGold());
+    }
+
+    @Test
+    void testExecuteNegative(){
+        goldAction = new GoldAction(-10);
+        goldAction.execute(player);
+        assertEquals(40, player.getGold());
+    }
 }
+
+
