9bf7a0a7dc5b1a6f8b8acdc261727ea6d25b8d30
Nicolaao <nicolaao@stud.ntnu.no>
2023-05-02 10:41:26 +0200

    Finished FileHandler.readStory method.

--- src/main/java/no/ntnu/idatg2001/paths/utility/FileHandler.java
+++ src/main/java/no/ntnu/idatg2001/paths/utility/FileHandler.java
@@ -1,10 +1,11 @@
 package no.ntnu.idatg2001.paths.utility;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import no.ntnu.idatg2001.paths.Link;
 import no.ntnu.idatg2001.paths.Passage;
 import no.ntnu.idatg2001.paths.Story;
@@ -21,48 +22,41 @@ public class FileHandler {
    */
   public static Story readStory(String filename) throws IOException {
     Story story = null;
-    String title = "";
-    List passages = new ArrayList();
+    String title;
+    Passage newPassage;
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-      title = reader.readLine().trim();
+    try (Scanner scanner = new Scanner(new File(filename))) {
+      title = scanner.nextLine().trim();
 
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine().trim();
 
-      String line = reader.readLine();
-      while (line != null) {
-        line.trim();
-        if (line.isEmpty()) {
-          continue;
-        }
-
-        // If the line is a passage, go into the if statment, and read the passage and its links
-        if (line.startsWith("::")) { // If the line is a passage,
+        // Check for passage start
+        if (line.startsWith("::")) {
           String passageTitle = line.substring(2).trim();
           StringBuilder contentBuilder = new StringBuilder();
           List<Link> links = new ArrayList<>();
 
-          // Read the passage and its links
-          while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            if (line.startsWith("[")) { // If the line is a link, add it to the list of links
-              // The link title is the text between the brackets
+          while (scanner.hasNextLine() && !(line = scanner.nextLine().trim()).isEmpty()) {
+            if (line.startsWith("[")) {
               String linkTitle = line.substring(1, line.indexOf("]"));
-              // The link passage is the text between the parentheses
               String linkPassage = line.substring(line.indexOf("(") + 1, line.indexOf(")"));
-              // Add the link to the list of links TODO: rework this, or make sure it is correct.
               links.add(new Link(linkTitle, linkPassage));
             } else {
               contentBuilder.append(line);
             }
           }
+          newPassage = new Passage(passageTitle, contentBuilder.toString());
+          for (Link link : links) {
+            newPassage.addLink(link);
+          }
 
-          // If the story already exists, add the passage to it, before finding the next passage
           if (story != null) {
-            story.addPassage(new Passage(passageTitle, contentBuilder.toString()));
-          } else { // If the story does not exist, make the story
-            story = new Story(title, new Passage(passageTitle, contentBuilder.toString()));
+            story.addPassage(newPassage);
+          } else {
+            story = new Story(title, newPassage);
           }
         }
-
       }
     }
     return story;
