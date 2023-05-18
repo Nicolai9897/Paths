d845bd8e5d87e5a0f54f0c1746c3c2d2f561b55a
nicolaao <nicolaao@stud.ntnu.no>
2023-05-18 13:37:49 +0200

    Added testers to ScoreActionTest

--- src/test/java/no/ntnu/idatg2001/paths/Actions/ScoreActionTest.java
+++ src/test/java/no/ntnu/idatg2001/paths/Actions/ScoreActionTest.java
@@ -1,4 +1,23 @@
 package no.ntnu.idatg2001.paths.Actions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import no.ntnu.idatg2001.paths.Player;
+
 public class ScoreActionTest {
+
+  ScoreAction scoreAction;
+  Player player;
+
+  @org.junit.jupiter.api.BeforeEach
+  public void createScoreAction() {
+    player = new Player("Markus", 100, 20, 50);
+  }
+
+  @org.junit.jupiter.api.Test
+  void testExecute() {
+    scoreAction = new ScoreAction(100);
+    scoreAction.execute(player);
+    assertEquals(120, player.getScore());
+  }
 }
