fea598221cd38e74ca2c38d4cecf42ea90857543
andersbm <anderbm@stud.ntnu.no>
2023-05-19 00:39:54 +0200

    Working on UI. Created MenuBar method for a modular way of implementing menubar for all scenes.

--- src/main/java/no/ntnu/idatg2001/paths/ui/PathsApp.java
+++ src/main/java/no/ntnu/idatg2001/paths/ui/PathsApp.java
@@ -1,13 +1,15 @@
 package no.ntnu.idatg2001.paths.ui;
 
-
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class PathsApp extends Application {
 
@@ -20,10 +22,12 @@ public class PathsApp extends Application {
         Button continueButton = new Button("Continue");
         Label welcome = new Label("Welcome to Paths!");
 
-        VBox rootNode = new VBox(100, welcome, continueButton);
-        rootNode.setAlignment(Pos.CENTER);
-
-        Scene scene1 = new Scene(rootNode, 600, 600);
+        VBox scene1Node = new VBox(50, welcome, continueButton);
+        scene1Node.setAlignment(Pos.CENTER);
+        BorderPane pane1 = new BorderPane();
+        pane1.setTop(menuBar());
+        pane1.setCenter(scene1Node);
+        Scene scene1 = new Scene(pane1, 600, 600);
 
         primaryStage.setScene(scene1);
 
@@ -33,16 +37,32 @@ public class PathsApp extends Application {
 
         VBox scene2Node = new VBox(10, sceneTwoText, backButton);
         scene2Node.setAlignment(Pos.CENTER);
+        BorderPane pane2 = new BorderPane();
+        pane2.setTop(menuBar());
+        pane2.setCenter(scene2Node);
+        Scene scene2 = new Scene(pane2, 300, 300);
 
-        Scene scene2 = new Scene(scene2Node, 300, 300);
 
         // Actions for the buttons
         continueButton.setOnAction(e -> primaryStage.setScene(scene2));
         backButton.setOnAction(e -> primaryStage.setScene(scene1));
 
+
         primaryStage.show();
     }
 
+    public Node menuBar(){
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("File");
+        menuBar.getMenus().add(fileMenu);
+
+        MenuItem closeItem = new MenuItem("Close");
+        fileMenu.getItems().add(closeItem);
+        closeItem.setOnAction(e -> Platform.exit());
+
+        return menuBar;
+    }
 
 
     public static void appMain(String[] args) {
