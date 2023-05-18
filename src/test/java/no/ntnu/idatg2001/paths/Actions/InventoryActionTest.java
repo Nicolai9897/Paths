90fa51ef6d07e7d757b468e22c2d37c1a4d66ab2
nicolaao <nicolaao@stud.ntnu.no>
2023-05-18 13:37:11 +0200

    Added tester to InventoryActionTest.

--- src/test/java/no/ntnu/idatg2001/paths/Actions/InventoryActionTest.java
+++ src/test/java/no/ntnu/idatg2001/paths/Actions/InventoryActionTest.java
@@ -1,4 +1,27 @@
 package no.ntnu.idatg2001.paths.Actions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import no.ntnu.idatg2001.paths.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class InventoryActionTest {
+  InventoryAction inventoryAction;
+  Player player;
+
+  @BeforeEach
+  public void createInventoryAction(){
+    player = new Player("Markus", 100, 20, 50);
+  }
+
+  @Test
+  void testExecute(){
+    inventoryAction = new InventoryAction("sword");
+    inventoryAction.execute(player);
+    assertTrue(player.getInventory().contains("sword"));
+  }
+
+
 }
