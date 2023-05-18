632f5cc6bd2c08c89b4c6bf00efcfe43b8921012
andersbm <anderbm@stud.ntnu.no>
2023-05-18 22:09:30 +0200

    Testing out different ui configurations. Added 2 scenes and functionality for switching between them

--- src/main/java/no/ntnu/idatg2001/paths/ui/PathsApp.java
+++ src/main/java/no/ntnu/idatg2001/paths/ui/PathsApp.java
@@ -2,20 +2,48 @@ package no.ntnu.idatg2001.paths.ui;
 
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PathsApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Paths project");
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Paths");
+
+        //Configurations for the first scene
+        Button continueButton = new Button("Continue");
+        Label welcome = new Label("Welcome to Paths!");
+
+        VBox rootNode = new VBox(100, welcome, continueButton);
+        rootNode.setAlignment(Pos.CENTER);
+
+        Scene scene1 = new Scene(rootNode, 600, 600);
+
+        primaryStage.setScene(scene1);
+
+        // Configurations for second scene
+        Label sceneTwoText = new Label("Second scene");
+        Button backButton = new Button("Back");
+
+        VBox scene2Node = new VBox(10, sceneTwoText, backButton);
+        scene2Node.setAlignment(Pos.CENTER);
+
+        Scene scene2 = new Scene(scene2Node, 300, 300);
+
+        // Actions for the buttons
+        continueButton.setOnAction(e -> primaryStage.setScene(scene2));
+        backButton.setOnAction(e -> primaryStage.setScene(scene1));
+
         primaryStage.show();
     }
 
-    @Override
-    public void stop() throws Exception {
-        System.exit(0);
-    }
+
 
     public static void appMain(String[] args) {
         launch(args);
