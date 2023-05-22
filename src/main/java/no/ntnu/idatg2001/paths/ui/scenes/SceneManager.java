b90b990455f326a0058b6cfa1ffb95b7f39b6041
andersbm <anderbm@stud.ntnu.no>
2023-05-22 02:29:16 +0200

    GameOverScene and controller created

--- src/main/java/no/ntnu/idatg2001/paths/ui/scenes/SceneManager.java
+++ src/main/java/no/ntnu/idatg2001/paths/ui/scenes/SceneManager.java
@@ -6,10 +6,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
-import no.ntnu.idatg2001.paths.ui.controllers.EditGoalsController;
-import no.ntnu.idatg2001.paths.ui.controllers.EditPlayerController;
-import no.ntnu.idatg2001.paths.ui.controllers.GameController;
-import no.ntnu.idatg2001.paths.ui.controllers.MainMenuController;
+import no.ntnu.idatg2001.paths.ui.controllers.*;
 
 
 public class SceneManager {
@@ -20,12 +17,14 @@ public class SceneManager {
     private GameController gameController;
     private EditGoalsController editGoalsController;
     private EditPlayerController editPlayerController;
+    private GameOverController gameOverController;
 
     //Scenes
     private MainMenuScene mainMenuScene;
     private GameScene gameScene;
     private EditGoalsScene editGoalsScene;
     private EditPlayerScene editPlayerScene;
+    private GameOverScene gameOverScene;
 
     public SceneManager(Stage stage){
         this.stage = stage;
@@ -35,12 +34,14 @@ public class SceneManager {
         this.gameController = new GameController(this);
         this.editGoalsController = new EditGoalsController(this);
         this.editPlayerController = new EditPlayerController(this);
+        this.gameOverController = new GameOverController(this);
 
         //scenes
         mainMenuScene = new MainMenuScene(this, stage, mainMenuController);
         gameScene = new GameScene(this, stage, gameController);
         editGoalsScene = new EditGoalsScene(this, stage, editGoalsController);
         editPlayerScene = new EditPlayerScene(this, stage, editPlayerController);
+        gameOverScene = new GameOverScene(this, stage, gameOverController);
     }
 
     public void showMainMenuScene() {
@@ -61,6 +62,11 @@ public class SceneManager {
     public void showEditGoalScene() {
         stage.setScene(editGoalsScene.getScene());
         editGoalsScene.getScene().getRoot().requestFocus();
+    }
+
+
+    public void showGameOver() {
+        gameOverScene.setMessage(gameOverController.checkGameEndCondition());
     }
 
     public Stage getStage() {
